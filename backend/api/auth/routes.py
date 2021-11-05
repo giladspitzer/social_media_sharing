8805f66c807a90e22cc949ebe23de9866efc96db
Gilad Spitzer <giladspitzer@gmail.com>
2021-11-04 20:48:37 -0400

    #2 refresh token

--- backend/api/auth/routes.py
+++ backend/api/auth/routes.py
@@ -3,6 +3,7 @@ from backend.api.auth import bp as auth
 from backend.models import UserAccount
 import jwt
 from datetime import datetime, timedelta
+from flask_login import current_user, login_required
 
 
 @auth.route('/register', methods=['POST'])
@@ -55,3 +56,15 @@ def check_email():
     account_data = request.json
     print(account_data.keys())
     return jsonify('hi')
+
+
+@auth.route('/refresh', methods=['GET'])
+@login_required
+def refresh_token():
+    token = jwt.encode({
+        'sub': current_user.id,
+        'iat': datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=current_app.config.get('TOKEN_TIMEOUT'))},
+        current_app.config['SECRET_KEY']).decode()
+    return jsonify({"access_token": token,
+                    "access_token_expiry": current_app.config.get('TOKEN_TIMEOUT')})
